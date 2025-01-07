3865626088fecc157d0da3edb87a8e8052d36b81
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2025-01-07 14:42:30 -0700

    Added validation test for lo-sw-angular, to be expanded to other data products eventuallty

--- imap_processing/tests/codice/test_codice_l1a.py
+++ imap_processing/tests/codice/test_codice_l1a.py
@@ -7,12 +7,34 @@ import pytest
 import xarray as xr
 
 from imap_processing.cdf.utils import load_cdf, write_cdf
+from imap_processing.codice import constants
 from imap_processing.codice.codice_l1a import process_codice_l1a
 
 from .conftest import TEST_L0_FILE, VALIDATION_DATA
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+DESCRIPTORS = [
+    "hi-ialirt",
+    "lo-ialirt",
+    "hskp",
+    "lo-counters-aggregated",
+    "lo-counters-singles",
+    "lo-sw-priority",
+    "lo-nsw-priority",
+    "lo-sw-species",
+    "lo-nsw-species",
+    "lo-sw-angular",
+    "lo-nsw-angular",
+    "hi-counters-aggregated",
+    "hi-counters-singles",
+    "hi-omni",
+    "hi-sectored",
+    "hi-priority",
+    "lo-pha",
+    "hi-pha",
+]
 
 EXPECTED_ARRAY_SHAPES = [
     (),  # hi-ialirt  # TODO: Need to implement
@@ -33,27 +55,6 @@ EXPECTED_ARRAY_SHAPES = [
     (),  # hi-priority  # TODO: Need to implement
     (),  # lo-pha  # TODO: Need to implement
     (),  # hi-pha  # TODO: Need to implement
-]
-
-EXPECTED_LOGICAL_SOURCES = [
-    "imap_codice_l1a_hi-ialirt",
-    "imap_codice_l1a_lo-ialirt",
-    "imap_codice_l1a_hskp",
-    "imap_codice_l1a_lo-counters-aggregated",
-    "imap_codice_l1a_lo-counters-singles",
-    "imap_codice_l1a_lo-sw-priority",
-    "imap_codice_l1a_lo-nsw-priority",
-    "imap_codice_l1a_lo-sw-species",
-    "imap_codice_l1a_lo-nsw-species",
-    "imap_codice_l1a_lo-sw-angular",
-    "imap_codice_l1a_lo-nsw-angular",
-    "imap_codice_l1a_hi-counters-aggregated",
-    "imap_codice_l1a_hi-counters-singles",
-    "imap_codice_l1a_hi-omni",
-    "imap_codice_l1a_hi-sectored",
-    "imap_codice_l1a_hi-priority",
-    "imap_codice_l1a_lo-pha",
-    "imap_codice_l1a_hi-pha",
 ]
 
 EXPECTED_NUM_VARIABLES = [
@@ -120,7 +121,7 @@ def test_l1a_data_array_shape(test_l1a_data, index):
             assert processed_dataset[variable].data.shape == expected_shape
 
 
-@pytest.mark.parametrize("index", range(len(EXPECTED_LOGICAL_SOURCES)))
+@pytest.mark.parametrize("index", range(len(DESCRIPTORS)))
 def test_l1a_logical_sources(test_l1a_data, index):
     """Tests that the Logical source of the dataset is what is expected.
 
@@ -136,7 +137,7 @@ def test_l1a_logical_sources(test_l1a_data, index):
     """
 
     processed_dataset = test_l1a_data[index]
-    expected_logical_source = EXPECTED_LOGICAL_SOURCES[index]
+    expected_logical_source = f"imap_codice_l1a_{DESCRIPTORS[index]}"
 
     # Mark currently broken/unsupported datasets as expected to fail
     # TODO: Remove these once they are supported
@@ -172,14 +173,9 @@ def test_l1a_num_variables(test_l1a_data, index):
     assert len(processed_dataset) == EXPECTED_NUM_VARIABLES[index]
 
 
-@pytest.mark.skip("Awaiting validation data")
 @pytest.mark.parametrize("index", range(len(VALIDATION_DATA)))
 def test_l1a_data_array_values(test_l1a_data: xr.Dataset, index):
-    """Tests that the generated L1a CDF contents are valid.
-
-    Once proper validation files are acquired, this test function should point
-    to those. This function currently just serves as a framework for validating
-    files, but does not actually validate them.
+    """Tests that the generated L1a CDF data array contents are valid.
 
     Parameters
     ----------
@@ -189,15 +185,26 @@ def test_l1a_data_array_values(test_l1a_data: xr.Dataset, index):
         The index of the list to test
     """
 
-    generated_dataset = test_l1a_data
-    validation_dataset = load_cdf(VALIDATION_DATA[index])
+    descriptor = DESCRIPTORS[index]
 
-    # Ensure the processed data matches the validation data
-    for variable in validation_dataset:
-        assert variable in generated_dataset
-        if variable != "epoch":
+    # Mark currently broken/unsupported datasets as expected to fail
+    # TODO: Remove these once they are supported
+    if index in [0, 1, 15, 16, 17]:
+        pytest.xfail("Data product is currently unsupported")
+
+    if descriptor == "lo-sw-angular":
+        counters = getattr(
+            constants, f'{descriptor.upper().replace("-","_")}_VARIABLE_NAMES'
+        )
+        processed_dataset = test_l1a_data[index]
+        validation_dataset = load_cdf(VALIDATION_DATA[index])
+
+        # Until I receive updated validation data with appropriate shape, for
+        # now, compare the sum of the data arrays
+        for counter in counters:
             np.testing.assert_array_equal(
-                validation_dataset[variable].data, generated_dataset[variable].data[0]
+                getattr(processed_dataset, counter).data.sum(),
+                getattr(validation_dataset, counter).data.sum(),
             )
 
 
